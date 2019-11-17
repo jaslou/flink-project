0c1aa863a30d4613554420cf010dcb6218bd325a
jaslou <359023065@qq.com>
2019-11-17 18:18:17 +0800

    add kafka producer and consumer

--- src/main/java/com/jaslou/streaming/UserDefineSourceKafkaConsumer.java
+++ src/main/java/com/jaslou/streaming/UserDefineSourceKafkaConsumer.java
@@ -1,6 +1,5 @@
 package com.jaslou.streaming;
 
-import com.jaslou.assigner.UserBehaviorTimeAssigner;
 import com.jaslou.domin.UserBehavior;
 import com.jaslou.domin.UserDeserializationSchema;
 import com.jaslou.source.UserBehaviorSource;
@@ -8,32 +7,29 @@ import com.jaslou.util.PropertyUtil;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
-
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 
 import java.util.Properties;
 
 /**
- * send data to kafka
+ * receive data from kafka
  */
-public class UserDefineSourceKafkaProducer {
-
-    public static final String KAFKA_TOPIC = "flink_topic";
-
+public class UserDefineSourceKafkaConsumer {
     public static void main(String[] args) throws Exception {
-
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         env.getConfig().setAutoWatermarkInterval(1000L);
-        env.setParallelism(4);
 
         Properties properties = PropertyUtil.getKafkaProperties();
-        DataStreamSource<UserBehavior> userBehaviorDataStreamSource = env.addSource(new UserBehaviorSource());
-        userBehaviorDataStreamSource.assignTimestampsAndWatermarks(new UserBehaviorTimeAssigner(Time.seconds(5)))
-                .addSink(new FlinkKafkaProducer011<>(KAFKA_TOPIC, new UserDeserializationSchema(), properties));
+        DataStreamSource<UserBehavior> userBehaviorDataStreamSource = env.addSource(new FlinkKafkaConsumer011<UserBehavior>(
+                "flink_topic",
+                new UserDeserializationSchema(),
+                properties
+        ));
 
-        env.execute("The job with sending data to kafka Job ");
+        userBehaviorDataStreamSource.print();
+
+        env.execute("The job with  receiving data from kafka ");
+
     }
-
 }
