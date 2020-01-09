8dcb52e288abc7c3f6d6f007b4ed39613c2e43cb
jaslou <jaslouxzhl@163.com>
2020-01-09 17:42:31 +0800

    add generate data

---
+++ flink-project-example/src/main/java/com/jaslou/util/DataGenericUtil.java
@@ -0,0 +1,74 @@
+package com.jaslou.util;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class DataGenericUtil {
+    public static void main(String[] args) throws Exception {
+        String[] ip = {"11", "12", "25", "13", "122", "152", "172", "9", "144"};
+        String[] url = {
+                "https://tianchi.aliyun.com",
+                "www.baidu.com",
+                "https://ci.apache.org",
+                "http://chenyuzhao.me/",
+                "https://blog.csdn.net/u012369535/article/details/96317565",
+                "https://www.ibm.com/",
+                "https://wssp.hainan.gov.cn",
+                "http://gjj.hainan.gov.cn",
+                "http://ping.chinaz.com/agent.youngport.com.cn"
+        };
+        String[] method = {"POST","GET"};
+        Random random = new Random(10);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int j = 0; j < 1000000; j++) {
+            int i = random.nextInt(9);
+            int a = random.nextInt(9);
+            int b = random.nextInt(9);
+            int c = random.nextInt(9);
+            Date date = randomDate("2019-01-01","2019-01-02");
+            String dateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(date);
+            stringBuilder
+                    .append(ip[i] + "." + ip[a] + "." + ip[b] + "." + ip[c]).append("\t")
+                    .append(dateFormat).append("\t")
+                    .append(url[i]).append("\t")
+                    .append(method[i%2]).append("\n");
+        }
+        File file = new File("/home/hadoop/flink-project/hot-item/src/main/resources/apache.log");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(stringBuilder.toString());
+        bufferedWriter.close();
+
+
+    }
+
+    private static long random(long begin,long end){
+        long rtn = begin + (long)(Math.random() * (end - begin));
+        if(rtn == begin || rtn == end){
+            return random(begin,end);
+        }
+        return rtn;
+    }
+
+    private static Date randomDate(String beginDate, String endDate){
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            Date start = format.parse(beginDate);
+            Date end = format.parse(endDate);
+
+            if(start.getTime() >= end.getTime()){
+                return null;
+            }
+            long date = random(start.getTime(),end.getTime());
+            return new Date(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
