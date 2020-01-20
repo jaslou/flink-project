2544503ea9ae706f63922fc76a805d8f0b246a50
jaslou <jaslouxzhl@163.com>
2020-01-20 17:42:48 +0800

    update DataGenericUtil

--- flink-project-example/src/main/java/com/jaslou/util/DataGenericUtil.java
+++ flink-project-example/src/main/java/com/jaslou/util/DataGenericUtil.java
@@ -7,6 +7,40 @@ import java.util.Random;
 
 public class DataGenericUtil {
     public static void main(String[] args) throws Exception {
+        generateLoginRecord(100L);
+    }
+
+    public static void generateLoginRecord(Long size) throws Exception {
+        // userId, ipAddress, status, timestamp
+        String[] ip = {"11", "12", "25", "13", "122", "152", "172", "9", "144"};
+        String[] status = {"fail","success"};
+        Random random = new Random(10000);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int j = 0; j < size; j++) {
+            int i = random.nextInt(9);
+            int a = random.nextInt(9);
+            int b = random.nextInt(9);
+            int c = random.nextInt(9);
+            Date date = randomDate("2019-01-01","2019-01-02");
+//            String dateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(date);
+
+            stringBuilder.append(date.getTime()).append("\t")
+                    .append(random.nextInt(10000)).append("\t")
+                    .append(ip[i] + "." + ip[a] + "." + ip[b] + "." + ip[c]).append("\t")
+                    .append(status[random.nextInt(2)]).append("\n");
+        }
+        File file = new File("/home/hadoop/flink-project/user-behavior-analysis/src/main/resources/loginrecord.csv");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(stringBuilder.toString());
+        bufferedWriter.close();
+    }
+
+    public static void generateLogindata() throws Exception {
+        // ipAddress, timestamp, method, url
         String[] ip = {"11", "12", "25", "13", "122", "152", "172", "9", "144"};
         String[] url = {
                 "https://tianchi.aliyun.com",
@@ -43,8 +77,6 @@ public class DataGenericUtil {
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         bufferedWriter.write(stringBuilder.toString());
         bufferedWriter.close();
-
-
     }
 
     private static long random(long begin,long end){
