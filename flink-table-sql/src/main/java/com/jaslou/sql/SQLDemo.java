a60b5891c80658b9510455f9d4b46f9b80fe045c
jaslou <359023065@qq.com>
2020-04-22 23:57:47 +0800

    update Order class

--- flink-table-sql/src/main/java/com/jaslou/sql/SQLDemo.java
+++ flink-table-sql/src/main/java/com/jaslou/sql/SQLDemo.java
@@ -59,32 +59,6 @@ public class SQLDemo {
 
     }
 
-    /**
-     * Simple POJO.
-     */
-    public static class Order {
-        public Long user;
-        public String product;
-        public int amount;
-
-        public Order() {
-        }
-
-        public Order(Long user, String product, int amount) {
-            this.user = user;
-            this.product = product;
-            this.amount = amount;
-        }
-
-        @Override
-        public String toString() {
-            return "Order{" +
-                    "user=" + user +
-                    ", product='" + product + '\'' +
-                    ", amount=" + amount +
-                    '}';
-        }
-    }
 }
 
 
--- flink-table-sql/src/main/java/com/jaslou/sql/TableFunctionTest.java
+++ flink-table-sql/src/main/java/com/jaslou/sql/TableFunctionTest.java
@@ -89,34 +89,5 @@ public class TableFunctionTest {
             return Types.ROW(Types.STRING, Types.STRING);
         }
     }
-
-
-
-    /**
-     * Simple POJO.
-     */
-    public static class Order {
-        public Long user;
-        public String product;
-        public int amount;
-
-        public Order() {
-        }
-
-        public Order(Long user, String product, int amount) {
-            this.user = user;
-            this.product = product;
-            this.amount = amount;
-        }
-
-        @Override
-        public String toString() {
-            return "Order{" +
-                    "user=" + user +
-                    ", product='" + product + '\'' +
-                    ", amount=" + amount +
-                    '}';
-        }
-    }
 }
 
