82105eeef9cca0a68866d03ca32b200bc59f2729
jaslou <359023065@qq.com>
2022-05-26 13:50:50 +0800

    test_api

---
+++ flink-project-example/src/test/java/jaslou/TestUtil.java
@@ -0,0 +1,10 @@
+package jaslou;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+
+public class TestUtil {
+    public static void main(String[] args) {
+        ParameterTool params = ParameterTool.fromArgs(args);
+
+    }
+}
